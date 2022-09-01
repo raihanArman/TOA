e4f567a55dc4487388b37000cea53bc487a50a17
raihanArman <raihanarman8@gmail.com>
2022-09-01 13:59:50 +0700

    Change version ktlint

--- app/src/androidTest/java/com/randev/toa/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/randev/toa/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package com.randev.toa
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import junit.framework.Assert.assertEquals
-
 import org.junit.Test
 import org.junit.runner.RunWith
-
 
 /**
  * Instrumented test, which will execute on an Android device.
--- app/src/main/java/com/randev/toa/ui/theme/Theme.kt
+++ app/src/main/java/com/randev/toa/ui/theme/Theme.kt
@@ -1,3 +1,4 @@
+
 package com.randev.toa.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
