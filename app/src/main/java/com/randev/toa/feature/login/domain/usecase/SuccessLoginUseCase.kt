e0debb14be24ae5a4f9a3dbeb73c7f73df17e267
raihanArman <raihanarman8@gmail.com>
2022-09-10 23:09:27 +0700

    Fixing 10min debt

--- app/src/main/java/com/randev/toa/feature/login/domain/usecase/SuccessLoginUseCase.kt
+++ app/src/main/java/com/randev/toa/feature/login/domain/usecase/SuccessLoginUseCase.kt
@@ -6,7 +6,7 @@ import com.randev.toa.feature.login.domain.model.LoginResult
  * @author Raihan Arman
  * @date 10/09/22
  */
-class SuccessLoginCredentialsUseCase : CredentialsLoginUseCase {
+class SuccessCredentialsLoginUseCase : CredentialsLoginUseCase {
     override suspend fun invoke(email: Email, password: Password): LoginResult {
         return LoginResult.Success
     }
