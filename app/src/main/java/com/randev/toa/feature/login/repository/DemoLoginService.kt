fb66dd8fddfa63f24921b00fbba0ac7c0f705505
raihanArman <raihanarman8@gmail.com>
2022-09-19 10:34:17 +0700

    Adding inject annotation

--- app/src/main/java/com/randev/toa/feature/login/repository/DemoLoginService.kt
+++ app/src/main/java/com/randev/toa/feature/login/repository/DemoLoginService.kt
@@ -6,13 +6,14 @@ import com.randev.toa.feature.login.domain.model.Credentials
 import com.randev.toa.feature.login.domain.model.LoginResponse
 import com.randev.toa.feature.login.domain.model.RefreshToken
 import com.randev.toa.feature.login.domain.model.Token
+import javax.inject.Inject
 
 /**
  * @author Raihan Arman
  * @date 19/09/22
  */
 
-class DemoLoginService : LoginRepository {
+class DemoLoginService @Inject constructor() : LoginRepository {
     override suspend fun loginWithCredentials(credentials: Credentials): Result<LoginResponse> {
         val defaultToken = Token(
             AuthToken(""),
--- app/src/main/java/com/randev/toa/feature/login/repository/DemoTokenService.kt
+++ app/src/main/java/com/randev/toa/feature/login/repository/DemoTokenService.kt
@@ -1,13 +1,14 @@
 package com.randev.toa.feature.login.repository
 
 import com.randev.toa.feature.login.domain.model.Token
+import javax.inject.Inject
 
 /**
  * @author Raihan Arman
  * @date 19/09/22
  */
 
-class DemoTokenService : TokenRepository {
+class DemoTokenService @Inject constructor() : TokenRepository {
     override suspend fun storeAuthToken(token: Token) {
         // No
     }
