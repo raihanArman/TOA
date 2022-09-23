5f530de5244ed2a09eea85e4e43b82ede3bf55e1
raihanArman <raihanarman8@gmail.com>
2022-09-23 11:21:18 +0700

    Supporting edge to edge experience

--- app/src/main/java/com/randev/toa/MainActivity.kt
+++ app/src/main/java/com/randev/toa/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.randev.toa.feature.login.ui.LoginScreen
 import com.randev.toa.ui.theme.TOATheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +17,17 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            val systemUIController = rememberSystemUiController()
+            val useDarkIcons = MaterialTheme.colors.isLight
+
+            SideEffect {
+                systemUIController.setSystemBarsColor(
+                    color = Color.Transparent,
+                    darkIcons = useDarkIcons
+                )
+            }
+
             TOATheme {
                 LoginScreen(
                     onLoginCompleted = {
