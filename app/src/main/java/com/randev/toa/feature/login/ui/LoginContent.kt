69ebf7d0e00d71f836ea8ce07a03b80658343597
raihanArman <raihanarman8@gmail.com>
2022-09-23 10:42:47 +0700

    Update typography

--- app/src/main/java/com/randev/toa/feature/login/ui/LoginContent.kt
+++ app/src/main/java/com/randev/toa/feature/login/ui/LoginContent.kt
@@ -170,7 +170,9 @@ private fun PasswordInput(
         onTextChanged = onTextChanged,
         labelText = stringResource(R.string.pasword),
         errorMessage = errorMessage,
-        visualTransformation = PasswordVisualTransformation(),
+        visualTransformation = PasswordVisualTransformation(
+            '-'
+        ),
         enabled = enabled,
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Password
